6dd56a50cc882bdcdba3d64958edfb6e6f92d6c2
sb-synth <stefano.bosisio@synthesia.io>
2024-10-31 22:41:23 +0000

    move

--- rayon_matmul/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cargo clean
-cargo build --release
-./target/release/rayon_matmul --b 64 --t 1024 --c 768 --oc 768
-wait
-./target/release/rayon_matmul --b 64 --t 1024 --c 768 --oc 768
-wait
-./target/release/rayon_matmul --b 64 --t 1024 --c 768 --oc 768
-wait
